8cd34296467aa3e52e978996c41fbafec839f9a5
30202858 <sarim.ekram1@ucalgary.ca>
2025-02-26 12:35:40 -0700

    Working on connect 4 dummy code. Completed the first dummy function createBoard

--- src/gameLogic/connect4/Connect4.java
+++ src/gameLogic/connect4/Connect4.java
@@ -2,4 +2,15 @@ package gameLogic.connect4;
 
 public class Connect4 {
 
+    /**
+     * This function creates a Board using the row and columns inputted by the user.
+     * This will be called in the gui controller class after the user decides to play connect 4 and selecting the size.
+     * @param rows inputted by the user.
+     * @param columns inputted by the user.
+     * @return a 2D array filled with an integer value which will be considered for an empty spot.
+     */
+    public static int[][] createBoard(int rows, int columns){
+        return null;
+    }
+
 }
