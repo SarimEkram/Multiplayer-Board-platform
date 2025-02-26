e6f08899ac375e7e43d1340c5fb957767fc372a9
30202858 <sarim.ekram1@ucalgary.ca>
2025-02-26 15:51:06 -0700

    Made changes in the @param

--- src/gameLogic/connect4/ConnectBoard.java
+++ src/gameLogic/connect4/ConnectBoard.java
@@ -13,7 +13,7 @@ public class ConnectBoard {
 
     /**
      * This function creates a Board using the row and columns inputted by the user.
-     * This will be called in the gui controller class after the user decides to play connect 4 and selecting the size.
+     * This is called in the constructor after the user decides to play connect 4 and selecting the size.
      * @return a 2D array filled with an integer value which will be considered for an empty spot.
      */
     public static int[][] createBoard(){
@@ -22,19 +22,17 @@ public class ConnectBoard {
 
     /**
      * This function counts the number of rows present in the board. This can be used in other functions of connect4 class
-     * @param board a 2D array board of size rows dimension 1 and columns dimension 2
      * @return the length of the board which is the length of the number of rows.
      */
-    public static int rowCount(int[][] board){
+    public static int rowCount(){
         return row;
     }
 
     /**
      * This function counts the number of columns present in the board. This can be used in other functions of connect4 class
-     * @param board a 2D array board of size rows dimension 1 and columns dimension 2
      * @return the length of the column which is the length of the number of columns.
      */
-    public static int columnCount(int[][] board){
+    public static int columnCount(){
         return column;
     }
 
