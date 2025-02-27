119f0472abd23da73f1c6d297cb1d47e0851de4d
Keshav Bhardwaj <keshav.bhardwaj1@ucalgary.ca>
2025-02-26 22:15:09 -0700

    added main method back

--- src/gameLogic/tictactoe/TicTacToe.java
+++ src/gameLogic/tictactoe/TicTacToe.java
@@ -16,27 +16,27 @@ public class TicTacToe {
     /**
      * The main method that starts the game.
      *
-     *
+     * @param args Command-line arguments (not used).
      */
-    public void game() {
+    public static void main(String[] args) {
     }
 
     /**
      * Handles a player's move.
      */
-    public void playMove() {
+    public  void playMove() {
     }
 
     /**
      * Switches the active player between 'X' and 'O'.
      */
-    public void changeActivePlayer() {
+    public  void changeActivePlayer() {
     }
 
     /**
      * Allows the current player to forfeit the game.
      */
-    public void forfeitGame() {
+    public  void forfeitGame() {
 
     }
     
