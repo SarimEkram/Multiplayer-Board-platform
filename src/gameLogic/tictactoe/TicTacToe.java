b271fc9ba70b56f67ea91b2273ccc7efdcac04b1
Keshav Bhardwaj <keshav.bhardwaj1@ucalgary.ca>
2025-02-26 22:13:53 -0700

    Removed static from main and renamed it and removed static from other methods

--- src/gameLogic/tictactoe/TicTacToe.java
+++ src/gameLogic/tictactoe/TicTacToe.java
@@ -16,27 +16,27 @@ public class TicTacToe {
     /**
      * The main method that starts the game.
      *
-     * @param args Command-line arguments (not used).
+     *
      */
-    public static void main(String[] args) {
+    public void game() {
     }
 
     /**
      * Handles a player's move.
      */
-    public static void playMove() {
+    public void playMove() {
     }
 
     /**
      * Switches the active player between 'X' and 'O'.
      */
-    public static void changeActivePlayer() {
+    public void changeActivePlayer() {
     }
 
     /**
      * Allows the current player to forfeit the game.
      */
-    public static void forfeitGame() {
+    public void forfeitGame() {
 
     }
     
