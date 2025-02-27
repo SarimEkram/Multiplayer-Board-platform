5d74d6a0f7776a107bb5b8d7640535835c91d36d
30225885 <manavnarendrakumar.p@ucalgary.ca>
2025-02-27 14:31:33 -0700

    Added base leadership and matchmaking interface for all games

---
+++ src/MatchmakingLeaderboard/IGameLeaderboard.java
@@ -0,0 +1,22 @@
+package MatchmakingLeaderboard;
+
+import java.util.Collection;
+import java.util.List;
+
+/**
+ *Abstract base class for game leaderboard and matchmaking
+ *
+ * @author Manav Patel
+ */
+
+public class IGameLeaderboard {
+
+    /**
+     * function to return
+     *
+     * @return
+     */
+    public List<?> getScores(){
+        return null;
+    }
+}
---
+++ src/MatchmakingLeaderboard/IGameMatchmaking.java
@@ -0,0 +1,11 @@
+package MatchmakingLeaderboard;
+
+/**
+ *Abstract base class for game leaderboard and matchmaking
+ *
+ * @author Manav Patel
+ */
+
+public class IGameMatchmaking {
+
+}
