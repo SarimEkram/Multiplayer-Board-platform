f24925d9be0cce3f21402e16ce5133586102b685
Keshav Bhardwaj <keshav.bhardwaj1@ucalgary.ca>
2025-02-26 21:20:29 -0700

    Initialised TicTacToe.java

---
+++ src/gameLogic/tictactoe/TicTacToe.java
@@ -0,0 +1,30 @@
+package gameLogic.tictactoe;
+
+import java.util.Scanner;
+
+public class TicTacToe {
+
+    private static TicTacToeBoard board;
+    private static char activePlayer;
+    private static Scanner scanner = new Scanner(System.in);
+
+    /**
+     * The main method that starts the game.
+     *
+     * @param args Command-line arguments (not used).
+     */
+    public static void main(String[] args) {
+    }
+
+    /**
+     * Handles a player's move.
+     */
+    public static void playMove() {
+    }
+
+    /**
+     * Switches the active player between 'X' and 'O'.
+     */
+    public static void changeActivePlayer() {
+    }
+}
