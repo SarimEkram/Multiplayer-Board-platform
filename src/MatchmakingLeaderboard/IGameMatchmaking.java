728500267a535e816dd69e8bbe3caee98163cdad
30242623 <happy.prajapati@ucalgary.ca>
2025-02-27 19:27:53 -0600

    changed it into Interface class

--- src/MatchmakingLeaderboard/IGameMatchmaking.java
+++ src/MatchmakingLeaderboard/IGameMatchmaking.java
@@ -6,79 +6,55 @@ package MatchmakingLeaderboard;
  * @author Manav Patel
  */
 
-public class IGameMatchmaking {
+public interface IGameMatchmaking {
 
-    void matchmaking(){
-//        findMatch();
-//        while (checkMatchmaking()) {
-//            Player player1 = getPlayer();
-//            Player player2 = getPlayer();
-//            if (checkPlayers(player1, player2)) {
-//                signalAddPlayer();
-//                signalAddPlayer();
-//                signalStartGame();
-//            }
-//        }
-    }
+    void matchmaking();
 
     /**
      * function for players to join the matchmaking queue
      *
      */
-    void joinQueue(){
-
-    }
+    void joinQueue();
 
     /**
      * function for players to leave the matchmaking queue
      *
      */
-    void leaveQueue(){
-    }
+    void leaveQueue();
 
     /**
      * function that contacts the server and finds matches to play
      *
      */
-    void findMatch(){
-    }
+    void findMatch();
 
     /**
      * function to signal start of game once matchmaking is completed
      *
      */
-    void signalStartGame(){
-    }
+    void signalStartGame();
 
     /**
      * function to check if the matchmaking is complete
      *
      * @return boolean that signals completion or failure
      */
-    boolean checkMatchmaking(){
-        return false;
-    }
+    boolean checkMatchmaking();
 
     /**
      * function to signal adding player to the game
      */
-    void signalAddPlayer(){
-    }
+    void signalAddPlayer();
 
     /**
      * function to simulate getting player from queue information for skilled based
      *
      */
-    Player getPlayer(){
-        return new Player(0.0,1,123456);
-    }
+    Player getPlayer();
 
     /**
      * Check if players are similar in skill level
-     * @param Player1
-     * @param Player2
+     *
      */
-    boolean checkPlayers(Object Player1, Object Player2){
-        return false;
-    }
+    boolean checkPlayers(int Player1, int Player2);
 }
