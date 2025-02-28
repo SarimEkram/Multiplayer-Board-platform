a9a13fb2ce9784b3a0a5f4d8b8d00041b148a045
30214813 <jayrajsinh.thakor@ucalgary.ca>
2025-02-28 13:46:03 -0700

    Updated Abstract Classes with dummy Override methods based on the Interfaces

--- src/MatchmakingLeaderboard/Checkers/Leaderboard/AbstractCheckersLeaderboard.java
+++ src/MatchmakingLeaderboard/Checkers/Leaderboard/AbstractCheckersLeaderboard.java
@@ -1,4 +1,29 @@
 package MatchmakingLeaderboard.Checkers.Leaderboard;
 
-public class AbstractCheckersLeaderboard implements IGameLeaderboard {
+import MatchmakingLeaderboard.IGameLeaderboard;
+import MatchmakingLeaderboard.Player;
+
+/**
+ * Abstract class for Checkers Leaderboard.
+ * Implements Game Leaderboard functionality.
+ *
+ * @author Jay Thakor
+ */
+public abstract class AbstractCheckersLeaderboard implements IGameLeaderboard {
+    @Override
+    public List<Player> getScores() {
+    }
+
+    @Override
+    public void updateScore(int playerId, int score) {
+    }
+
+    @Override
+    public List<Player> getTopPlayers() {
+    }
+
+    @Override
+    public void displayLeaderboard() {
+    }
+
 }
--- src/MatchmakingLeaderboard/Checkers/Matchmaking/AbstractCheckersMatchmaking.java
+++ src/MatchmakingLeaderboard/Checkers/Matchmaking/AbstractCheckersMatchmaking.java
@@ -1,4 +1,45 @@
 package MatchmakingLeaderboard.Checkers.Matchmaking;
 
-public class AbstractCheckersMatchmaking implements IGameMatchmaking{
+import MatchmakingLeaderboard.IGameMatchmaking;
+import MatchmakingLeaderboard.Player;
+
+/**
+ * Abstract class for Checkers Matchmaking
+ * Implements Game Matchmaking functionality.
+ *
+ * @author Jay Thakor
+ */
+public abstract class AbstractCheckersMatchmaking implements IGameMatchmaking {
+
+    @Override
+    public void joinQueue(int playerID) {
+    }
+
+    @Override
+    public void leaveQueue(int playerID) {
+    }
+
+    @Override
+    public void findMatch() {
+    }
+
+    @Override
+    public void signalStartGame() {
+    }
+
+    @Override
+    public boolean checkMatchmaking() {
+    }
+
+    @Override
+    public void signalAddPlayer() {
+    }
+
+    @Override
+    public Player getPlayer() {
+    }
+
+    @Override
+    public boolean checkPlayers(Player Player1, Player Player2) {
+    }
 }
