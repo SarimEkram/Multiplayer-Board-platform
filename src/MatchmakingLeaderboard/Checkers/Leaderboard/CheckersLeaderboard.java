ad85fd74cd42025ce1b826a621e4df89e6e3a30d
Neel Natvarbhai Savani <neelsavani44@gmail.com>
2025-02-28 20:42:18 -0700

    Added matchmaking methods to CheckersMatchmaking and  Leaderboard methods to CheckersLeaderboard

--- src/MatchmakingLeaderboard/Checkers/Leaderboard/CheckersLeaderboard.java
+++ src/MatchmakingLeaderboard/Checkers/Leaderboard/CheckersLeaderboard.java
@@ -1,4 +1,30 @@
 package MatchmakingLeaderboard.Checkers.Leaderboard;
 
+import MatchmakingLeaderboard.Player;
+
+import java.util.List;
+
+
 public class CheckersLeaderboard extends AbstractCheckersLeaderboard{
+
+    @Override
+    public List<Player> getScores() {
+        return List.of();
+    }
+
+    @Override
+    public void updateScore(int playerId, int score) {
+
+    }
+
+    @Override
+    public List<Player> getTopPlayers() {
+        return List.of();
+    }
+
+    @Override
+    public void displayLeaderboard() {
+
+    }
+
 }
