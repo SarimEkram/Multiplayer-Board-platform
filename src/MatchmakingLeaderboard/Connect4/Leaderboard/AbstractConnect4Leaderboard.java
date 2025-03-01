d81ef2c29492adc365f6ce403459090df822a0a0
30203774 <rahnuha.nurain@ucalgary.ca>
2025-03-01 00:26:19 -0700

    dummy code for AbstractConnect4Leaderboard

--- src/MatchmakingLeaderboard/Connect4/Leaderboard/AbstractConnect4Leaderboard.java
+++ src/MatchmakingLeaderboard/Connect4/Leaderboard/AbstractConnect4Leaderboard.java
@@ -25,7 +25,6 @@ public abstract class AbstractConnect4Leaderboard {
      * Records or updates a player's statistics in the leaderboard.
      */
     public void recordLeaderboardEntry(Player player, int score) {
-        // TODO: Add your leaderboard recording logic here
     }
 
     /**
