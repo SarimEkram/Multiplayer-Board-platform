4e88c12f7b915599b01b365bc5bbb88ddf028016
30220234 <abdulmajid.parappath@ucalgary.ca>
2025-03-25 18:16:04 -0600

    test generate

--- src/test/java/ca/ucalgary/groupprojectgui/HelloApplicationTest.java
+++ src/test/java/ca/ucalgary/groupprojectgui/HelloApplicationTest.java
@@ -1,4 +1,12 @@
+package ca.ucalgary.groupprojectgui;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class HelloApplicationTest {
-  
+
+    @Test
+    void main() {
+    }
 }
