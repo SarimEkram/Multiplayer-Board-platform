4d9ca5b6e321e7d92510d27bf91d5bb299fbb109
30220234 <abdulmajid.parappath@ucalgary.ca>
2025-03-26 05:02:10 -0600

    controller rename and dummy action

--- src/main/java/ca/ucalgary/groupprojectgui/p3/controllers/MainController.java
+++ src/main/java/ca/ucalgary/groupprojectgui/p3/controllers/MainController.java
@@ -3,7 +3,7 @@ package ca.ucalgary.groupprojectgui.p3.controllers;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class MainController {
     @FXML
     private Label welcomeText;
 
