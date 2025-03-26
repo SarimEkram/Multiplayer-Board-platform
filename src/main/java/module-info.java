a7400e8bafd76f6feafb50ef74b9e35b1ab65725
30220234 <abdulmajid.parappath@ucalgary.ca>
2025-03-25 18:58:26 -0600

    fix GUI not run

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,3 +1,4 @@
+module ca.ucalgary.groupprojectgui.p3 {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.web;
@@ -10,4 +11,6 @@
     requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
 
+    opens ca.ucalgary.groupprojectgui.p3 to javafx.fxml;
+    exports ca.ucalgary.groupprojectgui.p3;
 }
