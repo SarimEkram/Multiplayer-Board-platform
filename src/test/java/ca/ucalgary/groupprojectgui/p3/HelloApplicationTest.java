11aff87a1c272ccab3c35bdad1c2ef1f7be93c71
30220234 <abdulmajid.parappath@ucalgary.ca>
2025-03-26 00:49:22 -0600

    Junit test setup

---
+++ src/test/java/ca/ucalgary/groupprojectgui/p3/HelloApplicationTest.java
@@ -0,0 +1,12 @@
+package ca.ucalgary.groupprojectgui.p3;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloApplicationTest {
+
+    @Test
+    void main() {
+    }
+}
