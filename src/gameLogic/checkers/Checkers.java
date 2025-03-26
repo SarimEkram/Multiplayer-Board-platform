a44481cea62f2820c2f81caa1faf25852103b87f
Hardik Mehta <hardik.mehta2@ucalgary.ca>
2025-03-26 17:23:21 -0600

    combined the player and move class

--- src/gameLogic/checkers/Checkers.java
+++ src/gameLogic/checkers/Checkers.java
@@ -8,8 +8,6 @@ public class Checkers {
 
     CheckersBoard board;
 
-    private CheckersPlayer redPlayer;
-    private CheckersPlayer blackPlayer;
     private Turn turn;
     private WINNER winner;
 
@@ -34,8 +32,6 @@ public class Checkers {
      */
     public Checkers(CheckersBoard board) {
         this.board = board;
-        this.redPlayer = new CheckersPlayer();
-        this.blackPlayer = new CheckersPlayer();
         this.turn = Turn.BLACK;
         this.winner = WINNER.NONE;
     }
--- src/gameLogic/checkers/CheckersMove.java
+++ src/gameLogic/checkers/CheckersMove.java
@@ -5,6 +5,20 @@ package gameLogic.checkers;
  */
 public class CheckersMove {
 
+    /**
+     * Determines and processes the available moves for a given checkers piece on the board.
+     *
+     * @param board the current state of the checkers board
+     * @param piece the checkers piece for which available moves are to be determined\
+     * @param row the row of the piece
+     * @param col the column of the piece
+     * 
+     * this function check the availaibel moves based on the piece type (normal moves forward only, kind moves forward or backward)
+     */
+    public static void availaibleMoves(CheckersBoard board, CheckersPiece piece, int row, int col) {
+        
+    }
+
 
     /**
      * Executes a  move for the given checkers piece on the specified board.
