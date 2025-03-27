2b76e57130bc109224f5bfb5edc2b0d1fecdf6bb
30267195 <hasanveer.kaur@ucalgary.ca>
2025-03-26 21:50:35 -0600

    completed move class for checkersMove

--- src/gameLogic/checkers/CheckersMove.java
+++ src/gameLogic/checkers/CheckersMove.java
@@ -13,10 +13,10 @@ public class CheckersMove {
      * @param row the row of the piece
      * @param col the column of the piece
      * 
-     * this function check the availaibel moves based on the piece type (normal moves forward only, kind moves forward or backward)
+     * this function check the availaible moves based on the piece type (normal moves forward only, king moves forward or backward)
      */
-    public static void availaibleMoves(CheckersBoard board, CheckersPiece piece, int row, int col) {
-        
+    public static void availableMoves(CheckersBoard board, CheckersPiece piece, int row, int col) {
+
     }
 
 
@@ -30,8 +30,31 @@ public class CheckersMove {
      * moves the piece from start to dest location 
      */
     public static void move(CheckersBoard board, CheckersPiece piece, int startRow, int startCol, int destRow, int destCol) {
-        
+        board.board[destRow][destCol] = piece;
+        board.board[startRow][startCol] = null;
+
+        // Check if this move was a jump over another piece
+        int rowDifference = destRow - startRow;
+        int colDifference = destCol - startCol;
+
+        if (Math.abs(rowDifference) == 2 && Math.abs(colDifference) == 2) {
+            // Find the piece in the middle and remove it (captured)
+            int capturedRow = startRow + rowDifference / 2;
+            int capturedCol = startCol + colDifference / 2;
+            board.removePiece(capturedRow, capturedCol);
+        }
+
+        // Promote to king if the piece reaches the opposite end
+        if (piece.getColour() == CheckersPiece.Colour.RED && destRow == 0) {
+            piece.promoteToKing();
+            System.out.println("RED piece became a KING!");
+        } else if (piece.getColour() == CheckersPiece.Colour.BLACK && destRow == 7) {
+            piece.promoteToKing();
+            System.out.println("BLACK piece became a KING!");
+        }
+
     }
+
 
     
 
