4fe31405f827058deb33f404e8c4e2f6d3eff09b
Neel Natvarbhai Savani <neelsavani44@gmail.com>
2025-03-29 14:13:19 -0600

    Started Connect4Leaderboard

--- src/MatchmakingLeaderboard/Connect4/Leaderboard/Connect4Leaderboard.java
+++ src/MatchmakingLeaderboard/Connect4/Leaderboard/Connect4Leaderboard.java
@@ -7,8 +7,18 @@ import MatchmakingLeaderboard.Player;
  *
  * @author Rahnuha Nurain
  */
-public class Connect4Leaderboard extends AbstractConnect4Leaderboard {
+    public class Connect4Leaderboard extends AbstractConnect4Leaderboard {
 
+    // A map to hold players score
+    private static Map<Player, Integer> playerScores = new HashMap<>();
+
+    // Return a list of players sorted by scores in descending order
+    @Override
+    public List<Player> getScores() {
+        return playerScores.entrySet().stream()
+                .sorted(Map.Entry.<Player, Integer>comparingByValue().reversed())
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
 
     @Override
     public void displayLeaderboard() {
