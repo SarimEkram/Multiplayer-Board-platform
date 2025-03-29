f386a346d1156a545f0aaedf9584a5be85c8faff
30203774 <rahnuha.nurain@ucalgary.ca>
2025-03-28 23:14:23 -0600

    finished TicTacToeLeaderboard.java

--- src/MatchmakingLeaderboard/TicTacToe/Leaderboard/TicTacToeLeaderboard.java
+++ src/MatchmakingLeaderboard/TicTacToe/Leaderboard/TicTacToeLeaderboard.java
@@ -7,34 +7,40 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class TicTacToeLeaderboard extends AbstractTicTacToeLeaderboard {
+    // A map to hold player scores
+    private static Map<Player, Integer> playerScores = new HashMap<>();
 
-    private Map<Player, Integer> playerScores = new HashMap<>();
+    // Update the score for a player based on whether they won a game
+    public static void  updatePlayer(Player player, boolean won) {
+        int currentScore = playerScores.getOrDefault(player, 0);
+        playerScores.put(player, won ? currentScore + 1 : currentScore);
+    }
 
+    // Return a list of players sorted by scores in descending order
     @Override
     public List<Player> getScores() {
         return playerScores.entrySet().stream()
                 .sorted(Map.Entry.<Player, Integer>comparingByValue().reversed())
                 .map(Map.Entry::getKey)
                 .collect(Collectors.toList());
-
     }
 
-    public static void updatePlayer(Player player, boolean Won){
-
-    }
-
+    // Optionally, implement to return just the top N players
     @Override
     public List<Player> getTopPlayers() {
-        return List.of();
+        return getScores().stream().limit(10).collect(Collectors.toList());
     }
 
+    // Display all players and their scores
     @Override
     public void displayLeaderboard() {
-
+        System.out.println("Leaderboard:");
+        getScores().forEach(player -> System.out.println(player.getName() + ": " + playerScores.get(player)));
     }
 
+    // Sorting logic is integrated into the getScores method
     @Override
     public void sortLeaderboard() {
-
+        // This method can remain empty as sorting is handled in getScores.
     }
 }
