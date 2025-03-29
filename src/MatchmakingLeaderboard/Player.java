53e9f7d1628368d355efc010c2e725b5730fdbf1
30203774 <rahnuha.nurain@ucalgary.ca>
2025-03-28 23:09:32 -0600

    started TicTacToeLeaderboard

--- src/MatchmakingLeaderboard/Player.java
+++ src/MatchmakingLeaderboard/Player.java
@@ -132,4 +132,8 @@ public class Player {
     public void spectateMatch(int gameid) {
         // Implementation needed
     }
+
+    public boolean getName() {
+        return false;
+    }
 }
--- src/MatchmakingLeaderboard/TicTacToe/Leaderboard/TicTacToeLeaderboard.java
+++ src/MatchmakingLeaderboard/TicTacToe/Leaderboard/TicTacToeLeaderboard.java
@@ -1,14 +1,21 @@
 package MatchmakingLeaderboard.TicTacToe.Leaderboard;
 
 import MatchmakingLeaderboard.Player;
-
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class TicTacToeLeaderboard extends AbstractTicTacToeLeaderboard {
 
+    private Map<Player, Integer> playerScores = new HashMap<>();
+
     @Override
     public List<Player> getScores() {
-        return List.of();
+        return playerScores.entrySet().stream()
+                .sorted(Map.Entry.<Player, Integer>comparingByValue().reversed())
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
     }
 
     public static void updatePlayer(Player player, boolean Won){
