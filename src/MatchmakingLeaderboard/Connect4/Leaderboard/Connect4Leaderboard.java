06939432f3918229fdd088417c1462c00963e677
Neel Natvarbhai Savani <neelsavani44@gmail.com>
2025-03-30 17:00:06 -0600

    changes in Connect4Leaderboard.java according to PLayer Database

--- src/MatchmakingLeaderboard/Connect4/Leaderboard/Connect4Leaderboard.java
+++ src/MatchmakingLeaderboard/Connect4/Leaderboard/Connect4Leaderboard.java
@@ -11,41 +11,65 @@ import java.util.stream.Collectors;
 /**
  * Concrete class for Connect-4 leaderboard management.
  * @author Rahnuha Nurain
+ * @author Neel Savani
  */
-public class Connect4Leaderboard extends AbstractConnect4Leaderboard {
+public class Connect4Leaderboard extends AbstractConnect4Leaderboard{
 
-    // A map to hold players' scores
-    private Map<Player, Integer> playerScores = new HashMap<>();
+    private static final Connect4Leaderboard instance = new Connect4Leaderboard();
 
-    // Return a list of players sorted by scores in descending order
+    private Connect4Leaderboard(){
+
+    }
+    public static Connect4Leaderboard getInstance() {
+        return instance;
+    }
     @Override
     public List<Player> getScores() {
-        return playerScores.entrySet().stream()
-                .sorted(Map.Entry.<Player, Integer>comparingByValue().reversed())
-                .map(Map.Entry::getKey)
-                .collect(Collectors.toList());
+        sortLeaderboard();
+        return new ArrayList<>(players);
     }
 
-    // This method retrieves the list of top 10 players based on score
-    public List<Player> getTopPlayers() {
-        List<Player> allPlayers = getScores(); // Already sorted the list in descending order
-        List<Player> topPlayers = new ArrayList<>();
-        for (int i = 0; i < 10 && i < allPlayers.size(); i++) {  // Iterate over the score list of all players
-            topPlayers.add(allPlayers.get(i));   // Adding players to topPlayer list
-        }
-        return topPlayers;
+    public static void updatePlayer(Player player, boolean Won, int gameType){
+        instance.addPlayer(player, gameType);
+        instance.sortLeaderboard();
     }
+
+
 
     @Override
     public void displayLeaderboard() {
-        System.out.println("Connect4 Leaderboard");
-        List<Player> sortedAllPlayersList = getScores();
-        int rank = 1;
-        for (Player player : sortedAllPlayersList) {
-            int score = playerScores.get(player);
-            System.out.printf("%d | UserID %d | Score: %d | Wins: %d | Losses: %d%n",
-                    rank, player.getUserID(), score, player.getWins(), player.getLosses());
-            rank++;
+        PlayerDatabase.loadPlayersFromCSV();
+        List<Player> Connect4Players = new ArrayList<>();
+
+        for(int currentID = 100000; currentID <= 999999; currentID++){
+            Player player = PlayerDatabase.getPlayerByUserID(currentID);
+            if(player != null && player.getMMR(gameType) > 0){
+                Connect4Players.add(player);
+            }
+        }
+
+        //CheckersPlayers.sort(p1,p2) -> Integer.compare(p2.getMMR(gameType), p1.getMMR(gameType)));
+        this.players = Connect4Players;
+        sortLeaderboard();
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("\n Connect4 Leaderboard \n");
+        sb.append(String.format("%-5s %-15s %-8s %-10s %-8s%n", "Rank", "Username", "Level", "Rank", "MMR"));
+        for (int i = 0; i < Connect4Players.size(); i++) {
+            Player p = Connect4Players.get(i);
+            sb.append(String.format("%-5d %-15s %-8d %-10s %-8d%n",
+                    i+1,
+                    p.getUsername(),
+                    p.getLevel(),
+                    p.getRank(gameType).getCurrentTier(),
+                    p.getMMR(gameType)));
         }
     }
+
+
+
+    public void sortLeaderboard() {
+        players.sort((p1, p2) -> Integer.compare(p2.getMMR(gameType), p1.getMMR(gameType)));
+    }
+
 }
