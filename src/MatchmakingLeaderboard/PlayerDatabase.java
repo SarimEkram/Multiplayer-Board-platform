f4f2691230a10c96aab102cf893646778ce31f06
Maharsh Yogeshkumar Patel <maharshyogeshkumar.p@ucalgary.ca>
2025-03-29 21:26:57 -0600

    Updated the PlayerDatabase.java

--- src/MatchmakingLeaderboard/PlayerDatabase.java
+++ src/MatchmakingLeaderboard/PlayerDatabase.java
@@ -57,4 +57,84 @@ public class PlayerDatabase {
             e.printStackTrace();
         }
     }
+
+    /**
+     * Saves or updates a player in memory and writes all players to the CSV.
+     *
+     * @param player the player to save
+     * @return true if successful
+     */
+    public static boolean savePlayer(Player player) {
+        int index = -1;
+        for (int i = 0; i < players.size(); i++) {
+            if (players.get(i).getUserID() == player.getUserID()) {
+                index = i;
+                break;
+            }
+        }
+
+        if (index >= 0) {
+            players.set(index, player);
+        } else {
+            players.add(player);
+        }
+
+        return saveAllToCSV();
+    }
+
+    /**
+     * Retrieves a player by their user ID.
+     */
+    public static Player getPlayerByUserID(int userID) {
+        return players.stream()
+                .filter(p -> p.getUserID() == userID)
+                .findFirst()
+                .orElse(null);
+    }
+
+    /**
+     * Deletes a player by user ID.
+     */
+    public static boolean deletePlayer(int userID) {
+        players.removeIf(p -> p.getUserID() == userID);
+        return saveAllToCSV();
+    }
+
+    /**
+     * Writes all player data back to the CSV file.
+     */
+    private static boolean saveAllToCSV() {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(FILE_PATH))) {
+            // CSV Header
+            bw.write("userID,level,winRatio1,winRatio2,winRatio3,mmr1,mmr2,mmr3,signal1,signal2,signal3,wins1,wins2,wins3,losses1,losses2,losses3,rank1,rank2,rank3\n");
+
+            for (Player p : players) {
+                bw.write(p.getUserID() + "," +
+                        p.getLevel() + "," +
+                        p.getWinRatio(1) + "," +
+                        p.getWinRatio(2) + "," +
+                        p.getWinRatio(3) + "," +
+                        p.getMMR(1) + "," +
+                        p.getMMR(2) + "," +
+                        p.getMMR(3) + "," +
+                        p.getGameSignal(1) + "," +
+                        p.getGameSignal(2) + "," +
+                        p.getGameSignal(3) + "," +
+                        p.getWins(1) + "," +
+                        p.getWins(2) + "," +
+                        p.getWins(3) + "," +
+                        p.getLosses(1) + "," +
+                        p.getLosses(2) + "," +
+                        p.getLosses(3) + "," +
+                        p.getRank(1).getRankingPoints() + "," +
+                        p.getRank(2).getRankingPoints() + "," +
+                        p.getRank(3).getRankingPoints() + "\n");
+            }
+
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
