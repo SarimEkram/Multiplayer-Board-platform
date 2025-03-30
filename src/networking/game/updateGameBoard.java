2fe6c8aa70da6b02a7afe967b74ebeba3b5f3528
30221386 <mahin.chonigara@ucalgary.ca>
2025-03-30 12:44:49 -0600

    Modified constructor and class objects for updateGameBoard.java

--- src/networking/game/updateGameBoard.java
+++ src/networking/game/updateGameBoard.java
@@ -1,33 +1,35 @@
 package networking.game;
 
-public class updateGameBoard extends GameNetworking {
+import gameLogic.checkers.CheckersBoard;
+import gameLogic.connect4.ConnectBoard;
+import gameLogic.tictactoe.TicTacToeBoard;
+
+public class updateGameBoard<T> extends GameNetworking {
+
+    //TODO: Remove enum GameType if not used. Also create multiple versions of constructor and methods for each game if needed.
+    private GameType gameType;
 
     private String gameId; // Unique identifier for the game session
-
-    //TODO: Create a field/parameter 'board' of the game board from the game logic team when available.
-    //TODO: If a default constructor is used, keep the parameters in the class methods for fetch and upload.
-    //TODO: If a constructor with fields is used, remove the parameters from the class methods.
+    private T board;
 
     /**
-     * A constructor to initialize gameId and board.
+     * A constructor to initialize gameId and board for Checkers.
      *
      * @param gameId A string to uniquely identify the current game from the server.
-     * @param //board The local game board object which we want to update.
+     * @param board The local game board object for CheckersBoard, ConnectBoard, or TicTacToeBoard; which we want to update.
      */
-    public updateGameBoard(String gameId /* , board */) {
+    public updateGameBoard(String gameId, T board) {
         super(gameId);
         this.gameId = gameId;
-        // Initialize the board field.
+        this.board = board;
     }
 
     /**
      * Fetch the game board state from the server and update the local board.
      *
-     * @param gameId A string to uniquely identify the current game from the server.
-     * @param //board The local game board object which we want to update.
      * @return A boolean value, true if successful, false otherwise.
      */
-    public boolean fetchGameBoard(String gameId /* , board */) {
+    public boolean fetchGameBoard() {
         // Search the server for the specific game using the gameId. Alternatively, the game board could also store gameId.
         // Fetch the current state of the game board from the server and update the local version of the game board.
         // Return true if successful, false otherwise. Alternatively, could throw an exception if unsuccessful.
@@ -37,11 +39,9 @@ public class updateGameBoard extends GameNetworking {
     /**
      * Upload the local game board state to the server.
      *
-     * @param gameId A string to uniquely identify the current game from the server.
-     * @param //board The local game board object which we want to update.
      * @return A boolean value, true if successful, false otherwise.
      */
-    public boolean uploadGameBoard(String gameId /* , board */) {
+    public boolean uploadGameBoard() {
         // Search the server for the specific game using the gameId. Alternatively, the game board could also store gameId.
         // Upload the local version of game board to the server.
         // Return true if successful, false otherwise. Alternatively, could throw an exception if unsuccessful.
@@ -76,4 +76,9 @@ public class updateGameBoard extends GameNetworking {
         System.out.println("Fetching board update from server...");
         // Simulate retrieving update from server
     }
+
+    private enum GameType {
+        CHECKERS, CONNECT4, TICTACTOE;
+    }
 }
+
