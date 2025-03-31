da92690c6e28a48c46a919b3e46dabb6a5a3e91a
Neel Natvarbhai Savani <neelsavani44@gmail.com>
2025-03-30 20:35:38 -0600

    changes in AbstractTicTacToeLeaderboard

--- src/MatchmakingLeaderboard/TicTacToe/Leaderboard/AbstractTicTacToeLeaderboard.java
+++ src/MatchmakingLeaderboard/TicTacToe/Leaderboard/AbstractTicTacToeLeaderboard.java
@@ -3,10 +3,39 @@ package MatchmakingLeaderboard.TicTacToe.Leaderboard;
 import MatchmakingLeaderboard.IGameLeaderboard;
 import MatchmakingLeaderboard.IGameMatchmaking;
 
+/**
+ * Abstract class for TicTacToe Leaderboard.
+ * Implements Game Leaderboard functionality.
+ *
+ */
 public abstract class AbstractTicTacToeLeaderboard implements IGameLeaderboard {
+    protected List<Player> players = new ArrayList<>();
+    protected static final int gameType = 1;
 
 
-    public void leaderboard(){
-        // add the Leaderboard logic and exception handling.
+    public void addPlayer(Player player, int gameType) {
+        if (player == null) {
+            throw new NullPointerException("player is null");
+        }
+        if (!players.contains(player)) {
+            players.add(player);
+        }
+    }
+
+    //public abstract void updatePlayer(Player player, boolean won);
+
+    public abstract List<Player> getScores();
+
+    //public abstract List<Player> getTopPlayers();
+
+    public Player findPLayerById(int playerId) {
+        for (Player player : players) {
+            if (player.getUserID() == playerId) {
+                return player;
+            }
+        }
+        return null;
     }
 }
+
+
