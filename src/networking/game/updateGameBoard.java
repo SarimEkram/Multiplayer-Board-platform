4b97c5098165505dbab7db4094248bc8f4358529
30221386 <mahin.chonigara@ucalgary.ca>
2025-03-30 21:02:20 -0600

    Completed code for updateGameBoard.java

--- src/networking/game/updateGameBoard.java
+++ src/networking/game/updateGameBoard.java
@@ -4,12 +4,10 @@ import gameLogic.checkers.CheckersBoard;
 import gameLogic.connect4.ConnectBoard;
 import gameLogic.tictactoe.TicTacToeBoard;
 
-public class updateGameBoard<T> extends GameNetworking {
+public class updateGameBoard<T> {
 
-    //TODO: Remove enum GameType if not used. Also create multiple versions of constructor and methods for each game if needed.
-    private GameType gameType;
-
-    private String gameId; // Unique identifier for the game session
+    private final GameServer gameServer = new GameServer(); // An object to simulate the role of a server and database
+    private final String gameId; // Unique identifier for the game session
     private T board;
 
     /**
@@ -19,7 +17,6 @@ public class updateGameBoard<T> extends GameNetworking {
      * @param board The local game board object for CheckersBoard, ConnectBoard, or TicTacToeBoard; which we want to update.
      */
     public updateGameBoard(String gameId, T board) {
-        super(gameId);
         this.gameId = gameId;
         this.board = board;
     }
@@ -30,10 +27,13 @@ public class updateGameBoard<T> extends GameNetworking {
      * @return A boolean value; true if successful, false otherwise.
      */
     public boolean fetchGameBoard() {
-        // Search the server for the specific game using the gameId. Alternatively, the game board could also store gameId.
-        // Fetch the current state of the game board from the server and update the local version of the game board.
-        // Return true if successful, false otherwise. Alternatively, could throw an exception if unsuccessful.
-        return true;
+        T fetchedBoard = (T) gameServer.getGameState(gameId);
+        if (fetchedBoard != null) {     //If the fetched game board is not null, fetch is successful and return true
+            board = (T) fetchedBoard;
+            return true;
+        }
+        else
+            return false;
     }
 
     /**
@@ -42,43 +42,11 @@ public class updateGameBoard<T> extends GameNetworking {
      * @return A boolean value; true if successful, false otherwise.
      */
     public boolean uploadGameBoard() {
-        // Search the server for the specific game using the gameId. Alternatively, the game board could also store gameId.
-        // Upload the local version of game board to the server.
-        // Return true if successful, false otherwise. Alternatively, could throw an exception if unsuccessful.
-        return true;
-    }
-
-    /**
-     * Establishes a network connection.
-     * This method should handle setting up the connection using WebSockets, HTTP, or another networking protocol.
-     */
-    @Override
-    public void establishConnection() {
-
-    }
-
-    /**
-     * Closes the network connection.
-     * This method should handle safely closing the connection and cleaning up resources.
-     */
-    @Override
-    public void closeConnection() {
-
-    }
-    @Override
-    public void sendGameUpdate(String update) {
-        System.out.println("Uploading board update to server: " + update);
-        // simulate WebSocket or HTTP call to push update to the server
-    }
-
-    @Override
-    public void receiveGameUpdate() {
-        System.out.println("Fetching board update from server...");
-        // Simulate retrieving update from server
-    }
-
-    private enum GameType {
-        CHECKERS, CONNECT4, TICTACTOE;
+        boolean success = gameServer.saveGameState(gameId, board);
+        if (success)
+            return true;
+        else
+            return false;
     }
 }
 
