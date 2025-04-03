4054da80f9501e64e1fb9d225dfb1bb54574b037
30221386 <mahin.chonigara@ucalgary.ca>
2025-04-02 19:24:38 -0600

    Created tests classes for UpdateGameBoard.java and GameServer.java

---
+++ Tests/networking/game/GameServerTest.java
@@ -0,0 +1,20 @@
+package networking.game;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameServerTest {
+
+    @Test
+    void saveGameState() {
+    }
+
+    @Test
+    void getGameState() {
+    }
+
+    @Test
+    void removeGame() {
+    }
+}
---
+++ Tests/networking/game/UpdateGameBoardTest.java
@@ -0,0 +1,14 @@
+package networking.game;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UpdateGameBoardTest {
+
+    @org.junit.jupiter.api.Test
+    void fetchGameBoard() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void uploadGameBoard() {
+    }
+}
