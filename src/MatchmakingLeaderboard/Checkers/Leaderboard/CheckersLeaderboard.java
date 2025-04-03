56ba7e7656a689019d9a6f3505a97d7d6b43b616
30242623 <happy.prajapati@ucalgary.ca>
2025-04-03 12:49:28 -0600

    minute changes in leaderboard class

--- src/MatchmakingLeaderboard/Checkers/Leaderboard/CheckersLeaderboard.java
+++ src/MatchmakingLeaderboard/Checkers/Leaderboard/CheckersLeaderboard.java
@@ -55,7 +55,7 @@ public class CheckersLeaderboard extends AbstractCheckersLeaderboard{
 
         StringBuilder sb = new StringBuilder();
         sb.append("\n Checkers Leaderboard \n");
-        sb.append(String.format("%-5s %-15s %-8s %-10s %-8s%n", "Rank", "Username", "Level", "Rank", "MMR"));
+        sb.append(String.format("%-5s %-15s %-8s %-10s %-8s%n", "Position", "Username", "Level", "Rank", "MMR"));
         for (int i = 0; i < CheckersPlayers.size(); i++) {
             Player p = CheckersPlayers.get(i);
             sb.append(String.format("%-5d %-15s %-8d %-10s %-8d%n",
