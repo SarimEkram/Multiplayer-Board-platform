6b1cebafcb5365f7d63b7aa199cf8c9d533e134a
30212344 <aditya.jasrotia@ucalgary.ca>
2025-04-05 14:05:11 -0600

    created test file for ScoreValidator.java

---
+++ src/networking/game/tests/ScoreValidatorTest.java
@@ -0,0 +1,12 @@
+package networking.game.tests;
+
+import networking.game.ScoreValidator;
+import org.junit.jupiter.api.*;
+
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ScoreValidatorTest {
+
+}
