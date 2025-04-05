ff9830c7eedf487f61b1b24caa860af1f40cb04f
30212344 <aditya.jasrotia@ucalgary.ca>
2025-04-05 14:48:55 -0600

    made  few tests file for TurnTimer.java

--- src/networking/game/tests/TurnTimerTest.java
+++ src/networking/game/tests/TurnTimerTest.java
@@ -3,12 +3,20 @@ package networking.game.tests;
 import networking.game.TurnTimer;
 import org.junit.jupiter.api.*;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TurnTimerTest {
     private TurnTimer timer;
 
     @BeforeEach
     void setUp() {
         timer = new TurnTimer("player1", 2); // 2 seconds
+    }
+
+    @Test
+    void testStartTimer() {
+        timer.startTimer();
+        assertFalse(timer.isTimeExpired()); // Should not be expired immediately
     }
 
 }
