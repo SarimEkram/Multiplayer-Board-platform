27d7aa85019980d4bb2c65b0d33f9bb1be7884e3
30223926 <ayushi.patil@ucalgary.ca>
2025-04-05 20:48:32 -0600

    Added Test Cases for TicTacToeBoard

--- test/gameLogic/tictactoe/TicTacToeBoardTest.java
+++ test/gameLogic/tictactoe/TicTacToeBoardTest.java
@@ -6,7 +6,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import gameLogic.tictactoe.TicTacToeBoard;
 
 public class TicTacToeBoardTest {
-
     private TicTacToeBoard board;
 
     @BeforeEach
@@ -16,31 +15,67 @@ public class TicTacToeBoardTest {
     }
 
     @Test
-    void testEmptyCell() {
+    public void testCreateEmptyCell() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                assertTrue(board.isCellEmpty(i, j));
+            }
+        }
     }
 
     @Test
-    void testPlacePiece() {
+    public void testPlacePiece() {
+        board.placePiece(0, 0, 'X');
+    }
+
+    @Test
+    public void testPlacePieceAndEmptyCell() {
+        asserTrue(board.isCellEmpty(0, 0));
+        board.placePiece(0, 0, 'X');
+        assertFalse(board.isCellEmpty(0, 0));
     }
 
     @Test
     public void testCheckRowWin() {
+        board.placePiece(2, 0, 'O');
+        board.placePiece(2, 1, 'O');
+        board.placePiece(2, 2, 'O');
+        assertTrue(board.checkForWin('O'));
     }
 
     @Test
     public void testCheckColumnWin() {
-
+        board.placePiece(0, 0, 'X');
+        board.placePiece(1, 0, 'X');
+        board.placePiece(2, 0, 'X');
+        assertTrue(board.checkForWin('X'));
     }
 
     @Test
     public void testCheckDiagonalWin() {
+        board.placePiece(0, 2, 'O');
+        board.placePiece(1, 1, 'O');
+        board.placePiece(2, 0, 'O');
+        assertTrue(board.checkForWin('O'));
 
     }
 
     @Test
-    public void testBoardFull()
+    public void testBoardFull() {
+        char[][] pattern = {
+                {'X', 'O', 'X'},
+                {'O', 'X', 'O'},
+                {'O', 'X', 'O'},
+        }
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                board.placePiece(i, j, pattern[i][j]);
+        asserTrue(board.boardFull());
+    }
 
     @Test
-    public void testBoardNotFull() {
+    public void testNotFull() {
+        board.placePiece(1, 0, 'X');
+        assertFalse(board.boardFull());
     }
 }
