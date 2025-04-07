dfc3b5dba3d742aecdd2a25741de624860030805
Keshav Bhardwaj <keshav.bhardwaj1@ucalgary.ca>
2025-04-06 21:02:16 -0600

    Mentioned package for tictactoe test cases

--- test/gameLogic/tictactoe/TicTacToeBoardTest.java
+++ test/gameLogic/tictactoe/TicTacToeBoardTest.java
@@ -1,3 +1,4 @@
+package gameLogic.tictactoe;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
--- test/gameLogic/tictactoe/TicTacToeTest.java
+++ test/gameLogic/tictactoe/TicTacToeTest.java
@@ -1,3 +1,4 @@
+package gameLogic.tictactoe;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
