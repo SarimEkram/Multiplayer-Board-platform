740666f1670ca03b3b42fae29bad8ac370815c57
30202858 <sarim.ekram1@ucalgary.ca>
2025-04-09 01:28:00 -0600

    Now leaderboard is getting updated if a player wins by time

--- src/main/java/ca/ucalgary/groupprojectgui/p3/controllers/TicTacToeController.java
+++ src/main/java/ca/ucalgary/groupprojectgui/p3/controllers/TicTacToeController.java
@@ -564,6 +564,7 @@ public class TicTacToeController {
 //                    score2.setText("Score: " + scorePlayerO); // Update UI for Player O's score
                     gameProcessor.UpdateResults(opponentPlayer, localPlayer, gameType);
                 } else {
+                    //
                     // Player O's time expired, Player X wins
 //                    scorePlayerX++; // Increment score for Player X
 //                    score1.setText("Score: " + scorePlayerX); // Update UI for Player X's score
