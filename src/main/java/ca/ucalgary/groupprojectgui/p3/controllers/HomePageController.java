2d4e797dc01d9c5f1896652c8607a02e9a1cd098
Sahil Redhu <sahil.redhu1@ucalgary.ca>
2025-04-10 00:20:35 -0600

    Added auto refresh on add friend

--- src/main/java/ca/ucalgary/groupprojectgui/p3/controllers/HomePageController.java
+++ src/main/java/ca/ucalgary/groupprojectgui/p3/controllers/HomePageController.java
@@ -1130,6 +1130,7 @@ public class HomePageController {
             addFriendOverlay.setVisible(false);
             addFriendOverlay.setManaged(false);
             mainContainer.setEffect(null);
+            SceneManager.switchTo("/ca/ucalgary/groupprojectgui/p3/HomePage.fxml", "Home Page", "Home.css");
         });
         fadeOut.play();
     }
