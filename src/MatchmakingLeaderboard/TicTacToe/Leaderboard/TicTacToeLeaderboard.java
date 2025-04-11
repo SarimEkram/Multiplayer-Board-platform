d3e2dab5151445003cee0669ff7703f231f8ab31
30242623 <happy.prajapati@ucalgary.ca>
2025-04-10 22:22:49 -0600

    comments added in these files

--- src/MatchmakingLeaderboard/TicTacToe/Leaderboard/TicTacToeLeaderboard.java
+++ src/MatchmakingLeaderboard/TicTacToe/Leaderboard/TicTacToeLeaderboard.java
@@ -11,25 +11,44 @@ public class TicTacToeLeaderboard extends AbstractTicTacToeLeaderboard {
 
     private static final TicTacToeLeaderboard instance = new TicTacToeLeaderboard();
 
-
+    /**
+     * Private constructor to enforce Singleton pattern.
+     */
     private TicTacToeLeaderboard() {
     }
 
+    /**
+     * Provides access to the Singleton instance of the Checkers leaderboard.
+     */
     public static TicTacToeLeaderboard getInstance() {
         return instance;
     }
 
+    /**
+     * Retrieves the current leaderboard scores.
+     */
     @Override
     public List<Player> getScores() {
         sortLeaderboard();
         return new ArrayList<>(players);
     }
 
+    /**
+     * Updates a player's standing in the leaderboard.
+     * Adds the player to the leaderboard and triggers a re-sort.
+     * @param player
+     * @param Won
+     * @param gameType
+     */
     public static void updatePlayer(Player player, boolean Won, GameType gameType) {
         instance.addPlayer(player, gameType);
         instance.sortLeaderboard();
     }
 
+    /**
+     * Displays the current leaderboard by loading player data from CSV,
+     * filtering for Checkers players, and sorting by MMR.
+     */
     @Override
     public void displayLeaderboard() {
         PlayerDatabase.loadPlayersFromCSV();
@@ -46,6 +65,9 @@ public class TicTacToeLeaderboard extends AbstractTicTacToeLeaderboard {
         sortLeaderboard();
     }
 
+    /**
+     * Sorts the leaderboard in descending order of players' Checkers MMR.
+     */
     public void sortLeaderboard() {
         players.sort((p1, p2) -> Integer.compare(p2.getMMR(TIC_TAC_TOE), p1.getMMR(TIC_TAC_TOE)));
     }
